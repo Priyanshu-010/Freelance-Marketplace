b3f271dd8fa29d9d792e8e9b59bbed57ba9cab6d
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-16 17:18:41 +0530

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Navbar.scss'
+import { Link } from 'react-router-dom'
+
 
 const Navbar = () => {
+  const [active, setActive] = useState(false)
+  const [open, setOpen] = useState(false)
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.addEventListener("scroll", isActive);
+    }
+  }, [])
+  const currentUser = {
+    id: 1,
+    username: "Priyanshu",
+    isSeller: true
+  }
+
   return (
-    <div className='navbar'>
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
+          {/* <Link to="/"> */}
           <span className='text'>FreeLance</span>
+          {/* </Link> */}
+
           <span className='dot'>.</span>
         </div>
         <div className="links">
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => { setOpen(!open) }}>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs5JDAr96yjWS2NmmHMWsya_SKBdkQe5rxGQ&s" alt="" />
+              <span>{currentUser?.username}</span>
+              {open && <div className="options">
+                {
+                  currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add new Gig</span>
+
+                    </>
+                  )}
+                <span>Orders</span>
+                <span>Messages</span>
+                <span>Logout</span>
+              </div>}
+            </div>
+          )}
         </div>
       </div>
-    </div>
+      {
+        active && (
+          <>
+            <hr />
+            <div className="menu">
+              <span>Test</span>
+              <span>Test2</span>
+            </div>
+          </>
+        )
+      }
+    </div >
   )
 }
 
