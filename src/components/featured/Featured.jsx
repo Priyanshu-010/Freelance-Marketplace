2f66ddf20e69858928ebec1256b6f03cff409b0e
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-17 22:49:55 +0530

    Features

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Featured.scss'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className="container">
+        <div className="letf">
+          <h1>Find the perfect freelance services for your Business</h1>
+          <div className="search"></div>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
