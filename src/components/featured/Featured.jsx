42cec3685e4f3167a605337045a08865596af63e
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-19 20:54:54 +0530

    finalizied the image and started working for another component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
-import './Featured.scss'
+import React from "react";
+import "./Featured.scss";
 
-const Featured = () => {
+function Featured() {
   return (
-    <div className='featured'>
+    <div className="featured">
       <div className="container">
-        <div className="letf">
-          <h1>Find the perfect freelance services for your Business</h1>
+        <div className="left">
+          <h1>
+            Find the perfect <span>freelance</span> services for your business
+          </h1>
           <div className="search">
             <div className="searchInput">
               <img src="./img/search.png" alt="" />
-              <input type="text" placeholder='Try building mobile app' />
+              <input type="text" placeholder='Try "building mobil app"' />
             </div>
             <button>Search</button>
           </div>
           <div className="popular">
             <span>Popular:</span>
             <button>Web Design</button>
-            <button>Wordpress</button>
-            <button>Logo design</button>
+            <button>WordPress</button>
+            <button>Logo Design</button>
             <button>AI Services</button>
           </div>
         </div>
         <div className="right">
-          <img src="./img/newest_img.png" />
+          <img src="./img/newest_img_enhanced.png" alt="" />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Featured
+export default Featured;
---
+++ src/components/trustedBy/TrustedBY.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TrustedBy.scss'
+
+const TrustedBY = () => {
+  return (
+    <div>TrustedBY</div>
+  )
+}
+
+export default TrustedBY
