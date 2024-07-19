869ca4faef268a2e9f2ded4a2a633baf04ca6a91
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-19 21:04:07 +0530

    trusted by section completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.scss'
 import Featured from '../../components/featured/Featured'
+import TrustedBY from '../../components/trustedBy/TrustedBY'
 
 
 const Home = () => {
   return (
     <div>
       <Featured />
+      <TrustedBY />
     </div>
   )
 }
