c573caa179c05fe66a70ef380aad753c27c96d57
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-20 22:54:57 +0530

    infinite carousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Home.scss'
 import Featured from '../../components/featured/Featured'
 import TrustedBY from '../../components/trustedBy/TrustedBY'
+import Slide from '../../components/slide/Slide'
+import CatCard from "../../components/catCard/CatCard";
+import { cards } from '../../data'
 
 
 const Home = () => {
@@ -9,6 +12,11 @@ const Home = () => {
     <div>
       <Featured />
       <TrustedBY />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   )
 }
