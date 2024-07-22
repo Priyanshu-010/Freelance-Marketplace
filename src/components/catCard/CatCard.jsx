025cd01a2d3617bc4a7f5313b537cf07bfc05004
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-22 17:11:51 +0530

    only footer remaining in home.jsx

--- src/components/catCard/CatCard.jsx
+++ src/components/catCard/CatCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./CatCard.scss";
 
 function CatCard({ card }) {
-  console.log(card);
+
 
   if (!card || !card.img || !card.desc || !card.title) {
     return <div>Error: Card data is missing or incomplete</div>;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,8 @@ import Featured from '../../components/featured/Featured'
 import TrustedBY from '../../components/trustedBy/TrustedBY'
 import Slide from '../../components/slide/Slide'
 import CatCard from "../../components/catCard/CatCard";
-import { cards } from '../../data'
+import { cards, projects } from '../../data'
+import ProjectCard from '../../components/projectCard/ProjectCard'
 
 
 const Home = () => {
@@ -30,21 +31,21 @@ const Home = () => {
             </p>
             <div className="title">
               <img src="./img/check.png" alt="" />
-              The best for every budget
+              Quality work done quickly
             </div>
             <p>
-              Find high-quality services at every price point.
-            </p>
+              Find the right freelancer to begin working on your project within
+              minutes.            </p>
             <div className="title">
               <img src="./img/check.png" alt="" />
-              The best for every budget
+              Protected payments, every time
             </div>
             <p>
-              Find high-quality services at every price point.
-            </p>
+              Always know what you'll pay upfront. Your payment isn't released
+              until you approve the work.            </p>
             <div className="title">
               <img src="./img/check.png" alt="" />
-              The best for every budget
+              24/7 support
             </div>
             <p>
               Find high-quality services at every price point.
@@ -59,6 +60,99 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="explore">
+        <div className="container">
+          <h1>Explore the marketplace</h1>
+          <div className="items">
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Graphics & Design</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg"
+                alt=""
+              />
+              <div className="line"></div>
+
+              <span>Digital Marketing</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Writing & Translation</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Video & Animation</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Music & Audio</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Programming & Tech</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Business</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Lifestyle</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Data</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/photography.01cf943.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Photography</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   )
 }
