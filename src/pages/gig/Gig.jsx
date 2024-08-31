7e86a77f0d7189701cc21258ce01bc7162f61340
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-08-31 22:36:47 +0530

    single gig

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "./Gig.scss";
 import { Slider } from "infinite-react-carousel/lib";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 function Gig() {
+
+  const {id} =useParams();
+
+  
+
+  const { isPending, error, data, refetch } = useQuery({
+    queryKey: ['gig'],
+    queryFn: () =>
+      newRequest.get(`/gigs/single/${id}`).then((res) => {
+        return res.data;
+      })
+  })
   return (
     <div className="gig">
+      {isPending ? "loading" : error ? "Something went wrong" : 
       <div className="container">
         <div className="left">
           <span className="breadcrumbs">FreeLance.  Graphics & Design </span>
-          <h1>I will create ai generated art for you</h1>
+          <h1>{data.title}</h1>
           <div className="user">
             <img
               className="pp"
@@ -26,34 +42,17 @@ function Gig() {
             </div>
           </div>
           <Slider slidesToShow={1} arrowsScroll={1} className="slider">
-            <img
-              src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            {data.images.map((img) => (             
+              <img
+              key={img}
+              src={img}
               alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
+              />
+            ))}
           </Slider>
           <h2>About This Gig</h2>
           <p>
-            I use an AI program to create images based on text prompts. This
-            means I can help you to create a vision you have through a textual
-            description of your scene without requiring any reference images.
-            Some things I've found it often excels at are: Character portraits
-            (E.g. a picture to go with your DnD character) Landscapes (E.g.
-            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
-            team, business, profile picture) You can be as vague or as
-            descriptive as you want. Being more vague will allow the AI to be
-            more creative which can sometimes result in some amazing images. You
-            can also be incredibly precise if you have a clear image of what you
-            want in mind. All of the images I create are original and will be
-            found nowhere else. If you have any questions you're more than
-            welcome to send me a message.
+           {data.desc}
           </p>
           <div className="seller">
             <h2>About The Seller</h2>
@@ -238,44 +237,33 @@ function Gig() {
         </div>
         <div className="right">
           <div className="price">
-            <h3>1 AI generated image</h3>
-            <h2>$ 59.99</h2>
+            <h3>{data.shorTitle}</h3>
+            <h2>$ {data.price}</h2>
           </div>
           <p>
-            I will create a unique high quality AI generated image based on a
-            description that you give me
+            {data.shortDesc}
           </p>
           <div className="details">
             <div className="item">
               <img src="/img/clock.png" alt="" />
-              <span>2 Days Delivery</span>
+              <span>{data.deliveryDate} Days Delivery</span>
             </div>
             <div className="item">
               <img src="/img/recycle.png" alt="" />
-              <span>3 Revisions</span>
+              <span>{data.revisionNumber} Revisions</span>
             </div>
           </div>
           <div className="features">
-            <div className="item">
+            {data.features.map((feature) => (             
+              <div className="item" key={feature}>
               <img src="/img/greencheck.png" alt="" />
-              <span>Prompt writing</span>
+              <span>{feature}</span>
             </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Artwork delivery</span>
-            </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Image upscaling</span>
-            </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Additional design</span>
-            </div>
+            ))}
           </div>
           <button>Continue</button>
         </div>
-      </div>
+      </div>}
     </div>
   );
 }
